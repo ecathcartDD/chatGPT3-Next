39accb7eba8852da7e1b7a9386bc24a35bdefee3
eddycat-1 <edward.a.j.cathcart@gmail.com>
2023-03-15 21:40:49 +1100

    App

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,41 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(req, res) {
+  const prompt = req.query.prompt;
+
+  if (!prompt) {
+    return res.status(400).json({ error: "Prompt missing" });
+  }
+
+  if (prompt.length > 100) {
+    return res.status(400).json({ error: "Prompt too long" });
+  }
+
+  const completion = await openai.createCompletion({
+    model: "text-curie-001",
+    prompt: `"I want you to act as a song recommender. 
+    I will provide you with a song and you will create a 
+    playlist of 10 songs that are similar to the given song. 
+    You should also provide a name for the playlist. 
+    Do not choose songs that same name or artist. 
+    Do not write any explanations or other words, just reply with 
+    the playlist name, description and the songs. The song is ${prompt}"`,
+    max_tokens: 500,
+    temperature: 1,
+    presence_penalty: 0,
+    frequency_penalty: 0,
+  });
+
+  const quote = completion.data.choices[0].text;
+
+  return res.status(200).json({ quote });
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,37 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import { Button, Form, Spinner } from "react-bootstrap";
+import { FormEvent, useState } from "react";
 
 export default function Home() {
+  const [quote, setQuote] = useState("");
+  const [quoteLoading, setQuoteLoading] = useState(false);
+  const [quoteLoadingError, setQuoteLoadingError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const prompt = formData.get("prompt")?.toString().trim();
+
+    if (prompt) {
+      try {
+        setQuote("");
+        setQuoteLoadingError(false);
+        setQuoteLoading(true);
+        const response = await fetch(
+          "/api/hello?prompt=" + encodeURIComponent(prompt)
+        );
+        const body = await response.json();
+        setQuote(body.quote);
+      } catch (error) {
+        console.log(error);
+        setQuoteLoadingError(true);
+      } finally {
+        setQuoteLoading(false);
+      }
+    }
+  };
+
   return (
     <>
       <Head>
@@ -15,109 +41,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+        <h1>My AI 🤖</h1>
+        <h2>Powered by GPT-3</h2>
+        <Form onSubmit={handleSubmit} className={styles.inputForm}>
+          <Form.Group className="mb-3" controlId="prompt-input">
+            <Form.Label>
+              <h3>Enter a Simple Prompt:</h3>
+            </Form.Label>
+            <Form.Control name="prompt" maxLength={100}></Form.Control>
+          </Form.Group>
+          <Button type="submit" className="mb-3">
+            Submit
+          </Button>
+        </Form>
+        {quoteLoading && <Spinner />}
+        {quoteLoadingError && (
+          <div className="alert alert-danger">
+            {"Something went wrong. Please try again"}
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        )}
+        {quote && <div className="fw-semibold fs-5">{quote}</div>}
       </main>
     </>
-  )
+  );
 }
